42bd85f1a33273503fbaf37e6ed51f5bbcc0e223
xkarina2197 <xkarina2197@gmail.com>
2025-11-14 14:39:43 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+# 1-misol
+matn = "  Python dasturlash  "
+print(len(matn))
+print(matn.lstrip())
+
+# 2-misol
+matn = "hello world"
+print(matn.upper())
+print(len(matn))
+
+# 3-misol
+matn = " SALOM DUNYO 123 "
+print(matn.lower())
+print(matn.strip())
+
+# 4-misool
+matn = "ozbekiston"
+print(matn.rstrip())
+print(matn.capitalize())
+
+# 5-misol
+matn = "python3.11"
+print(matn.title())
+print(len(matn))
+
+# 6-misol
+matn = "  Toshkent 2025  "
+print(matn.strip())
+print(matn.upper())
+
+
+# 7-misol
+matn = "ALGORITM VA DSTURLASH"
+print(matn.lower())
+print(matn.lstrip())
+
+
